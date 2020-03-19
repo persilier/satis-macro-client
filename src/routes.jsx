ab7da487c9cce74901dc7e693261a5a2dc64355d
onesine <philemonlewhe@gmail.com>
2020-03-19 10:10:03 +0100

    Initial commit

---
+++ src/routes.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const routes = () => {
+    return (
+        <h1>All route</h1>
+    );
+};
+
+export default routes;
---
+++ src/views/layouts/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Satis client 2020</h1>
+        </div>
+    );
+}
+
+export default App;
