7f950f98710fb869c2a78126e9a1c90fc1c087d2
onesine <philemonlewhe@gmail.com>
2020-04-24 18:55:16 +0100

    Probleme 1

--- src/config/appConfig.js
+++
@@ -1,9 +0,0 @@
-const appConfig =  {
-    version: "2020.1",
-    name: 'SATIS',
-    enterprise: 'DMD',
-    contact: '21256325',
-    apiDomaine: "http://127.0.0.1:8000"
-};
-
-export default appConfig;
--- src/routes.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const routes = () => {
-    return (
-        <h1>All route</h1>
-    );
-};
-
-export default routes;
