854e7ca2121251486135ba2db71e01285f3a4cfd
Estelle ODJO <estelle@dmdconsult.com>
2020-04-30 09:58:14 +0100

    no message

--- src/views/components/FaqListe.jsx
+++ src/views/components/FaqListe.jsx
@@ -66,7 +66,7 @@ const FaqListe = () => {
                                             ) : ''}
                                         </div>
                                     ))
-                                ) : "Le Tableau est vide"}
+                                ) : ""}
 
                             </div>
                         </div>
