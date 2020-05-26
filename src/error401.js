732b35ad5ef6e048e5d819b87d82c6dd031c7810
onesine <philemonlewhe@gmail.com>
2020-05-26 10:06:05 +0100

    Implemenation de unit, unit_type, position

---
+++ src/error401.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const error401 = () => {
+    return (
+        <p>
+            <h1>Error 401</h1>
+            <p>Vous n'avez pas la permission de voire cette page</p>
+        </p>
+    );
+};
+
+export default error401;
