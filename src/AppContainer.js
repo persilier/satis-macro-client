b63e5ca5d7203cf39d5e6cac86221894704eca73
onesine <philemonlewhe@gmail.com>
2020-08-25 11:05:50 +0100

    Correction port

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -14,7 +14,7 @@ window.Echo = new Echo({
     cluster: 'mt1',
     forceTLS: false,
     wsHost: appConfig.host,
-    wsPort: localStorage.getItem("plan") === "PRO" ? 6003 : 6001,
+    wsPort: appConfig.port,
     disableStats: true,
     authEndpoint: appConfig.apiDomaine+'/api/broadcasting/auth',
     auth: {
--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -6,6 +6,7 @@ const appConfig =  {
     contact: '21256325',
     apiDomaine: `http://satis-${localStorage.getItem('plan') ? localStorage.getItem('plan').toLowerCase() : ""}.local`,
     host: `satis-${localStorage.getItem('plan') ? localStorage.getItem('plan').toLowerCase() : ""}.local`,
+    port: localStorage.getItem("plan") === "PRO" ? 6003 : localStorage.getItem("plan") === "HUB" ? 6002 : 6001
 };
 
 export default appConfig;
