97c0c68b0add6657b415b20439dc9ac84be65fec
Estelle ODJO <estelle@dmdconsult.com>
2020-08-26 08:59:40 +0100

    satisfaction

--- src/views/pages/SatisfactionMeasure.jsx
+++ src/views/pages/SatisfactionMeasure.jsx
@@ -147,20 +147,15 @@ const SatisfactionMeasure = (props) => {
                 <td>{`${measure.claimer.lastname} ${measure.claimer.firstname}`}</td>
                 <td>{measure.description === null ? "" : measure.description}</td>
                 <td>{measure.active_treatment.solution === null ? "" : measure.active_treatment.solution}</td>
-                {
-                    verifyPermission(props.userPermissions, "update-satisfaction-measured-my-claim") ||
-                    verifyPermission(props.userPermissions, "update-satisfaction-measured-my-claim") ? (
-                        <td style={{textAlign: 'center'}}>
-                            <a href={`/process/claim_measure/${measure.id}/detail`}
-                               className="btn btn-sm btn-clean btn-icon btn-icon-md"
-                               title="Détail">
-                                <i className="la la-eye"/>
-                            </a>
+                
+                <td style={{textAlign: 'center'}}>
+                    <a href={`/process/claim_measure/${measure.id}/detail`}
+                          className="btn btn-sm btn-clean btn-icon btn-icon-md"
+                          title="Détail">
+                        <i className="la la-eye"/>
+                    </a>
 
-                        </td>
-                    ):""
-                }
-
+                </td>
             </tr>
         )
     };
