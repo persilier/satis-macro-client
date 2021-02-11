1fde3c74a4ba976aa9f6be9494fb030730ebd2f1
Estelle ODJO <estelle@dmdconsult.com>
2021-02-11 09:34:57 +0100

    no message

--- src/modules/login/views/Pages/LoginPage.jsx
+++ src/modules/login/views/Pages/LoginPage.jsx
@@ -150,7 +150,6 @@ const LoginPage = (props) => {
                             <div className="kt-grid kt-grid--ver kt-grid--root kt-page">
                                 <div className="kt-grid kt-grid--hor kt-grid--root  kt-login kt-login--v1"
                                      id="kt_login">
-                                    {console.log(componentData,"Component")}
 
                                     <div
                                         className="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--desktop kt-grid--ver-desktop kt-grid--hor-tablet-and-mobile">
