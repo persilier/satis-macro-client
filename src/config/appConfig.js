0ad75e930ce97a651e609a486ea482815359c127
Nelson <ulrich@dmdconsult.com>
2021-02-16 12:37:21 +0100

    update appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -6,9 +6,9 @@ const appConfig =  {
     enterprise: 'DMD',
     contact: '21256325',
     timeAfterDisconnection: 8,
-    host: `satis-${localStorage.getItem('plan') ? localStorage.getItem('plan').toLowerCase() : ""}.local`,
-    apiDomaine: `http://satis-pro.local`,
-    port: localStorage.getItem("plan") === "PRO" ? 6003 : localStorage.getItem("plan") === "HUB" ? 6002 : 6001,
+    host: `satis-macro.local`,
+    apiDomaine: `http://satis-macro.local`,
+    port: 6001,
 };
 
 export default appConfig;
