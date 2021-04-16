49d1a1d2fd0c50f5ff4c9a1b1de2110f62729d2a
AZONHOU Nelson <ulrich@dmdconsult.com>
2021-04-16 17:53:13 +0100

    Give permissions to files

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -5,9 +5,9 @@ const appConfig =  {
     enterprise: 'DMD',
     contact: '21256325',
     timeAfterDisconnection: 8,
-    apiDomaine:`http://satis-hub.local`,
+    apiDomaine:`http://satismacro.develop`,
     // apiDomaine: `http://satis-${localStorage.getItem('plan') ? localStorage.getItem('plan').toLowerCase() : ""}.local`,
-    host: `satis-hub.local`,
+    host: `satismacro.develop`,
     // host: `satis-${localStorage.getItem('plan') ? localStorage.getItem('plan').toLowerCase() : ""}.local`,
     port:  "PRO" ? 6003 : "HUB" ? 6002 : 6001
     // port: localStorage.getItem("plan") === "PRO" ? 6003 : localStorage.getItem("plan") === "HUB" ? 6002 : 6001
