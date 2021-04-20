25312f433efb599a7a753882760923b6e188b23b
odjo ida <estelle.odjo@gmail.com>
2021-04-20 09:02:05 +0100

    untrack .eslintcache

---
+++ src/views/components/DashboardForm/constante.js
@@ -0,0 +1,256 @@
+const responseData = {
+    satis: {
+        statistics: {
+            totalRegistered: {
+                allInstitution: 7,
+                myInstitution: 5
+            },
+            totalIncomplete: {
+                allInstitution: 0
+            },
+            myInstitution: 0
+        },
+        totalComplete: {
+            allInstitution: 2
+        }, myInstitution: 0
+    },
+    totalTransferredToUnit: {
+        allInstitution: 0
+        , myInstitution: 0
+    }, totalBeingProcess: {
+        allInstitution: 0, myInstitution: 0
+    },
+    totalTreated: {
+        allInstitution: 0, myInstitution: 0
+    },
+    totalUnfounded: {
+        allInstitution: 0, myInstitution: 0
+    },
+    totalMeasuredSatisfaction: {
+        allInstitution: 0, myInstitution: 0
+    },
+    channelsUse: {
+        SMS: {
+            allInstitution: 5, myInstitution: 5
+        },
+        APPELTELEPHONIQUE: {
+            allInstitution: 0
+            , myInstitution: 0
+        },
+        EMAIL: {
+            allInstitution: 2, myInstitution: 0
+        },
+        WEB: {
+            allInstitution: 0, myInstitution: 0
+        },
+        MOBILE: {
+            allInstitution: 0, myInstitution: 0
+        },
+        ENTRETIEN: {
+            allInstitution: 0, myInstitution: 0
+        }
+    },
+    claimObjectsUse: {
+        Relevé: {allInstitution: 1, myInstitution: 1},
+        Manque_respect: {allInstitution: 4, myInstitution: 4},
+        Relevé_mensuelnon_reçu: {allInstitution: 0, myInstitution: 0},
+        Demande_compte: {allInstitution: 0, myInstitution: 0},
+        Débit_GIM: {allInstitution: 2, myInstitution: 0}
+    },
+    claimerSatisfactionEvolution: {
+        mai_2020: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        juin_2020: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0}
+            , measured: {allInstitution: 0, myInstitution: 0}
+        },
+        juil_2020: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        août2020: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0}
+            , measured: {allInstitution: 0, myInstitution: 0}
+        },
+        sept_2020: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        oct2020: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        nov2020: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        déc2020: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        janv2021: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        févr2021: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        mars2021: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        avr2021: {
+            satisfied: {allInstitution: 0, myInstitution: 0},
+            unsatisfied: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        }
+    },
+    claimerProcessEvolution: {
+        mai2020: {
+            registered: {allInstitution: 0, myInstitution: 0},
+            transferred_to_unit: {allInstitution: 0, myInstitution: 0},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        juin2020: {
+            registered: {allInstitution: 0, myInstitution: 0},
+            transferred_to_unit: {allInstitution: 0, myInstitution: 0},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        juil2020: {
+            registered: {allInstitution: 0, myInstitution: 0},
+            transferred_to_unit: {allInstitution: 0, myInstitution: 0},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        août2020: {
+            registered: {allInstitution: 0, myInstitution: 0},
+            transferred_to_unit: {allInstitution: 0, myInstitution: 0},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        sept2020: {
+            registered: {allInstitution: 0, myInstitution: 0},
+            transferred_to_unit: {allInstitution: 0, myInstitution: 0},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        oct2020: {
+            registered: {allInstitution: 0, myInstitution: 0},
+            transferred_to_unit: {allInstitution: 0, myInstitution: 0},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        nov2020: {
+            registered: {allInstitution: 0, myInstitution: 0},
+            transferred_to_unit: {allInstitution: 0, myInstitution: 0},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        déc2020: {
+            registered: {allInstitution: 0, myInstitution: 0},
+            transferred_to_unit: {allInstitution: 0, myInstitution: 0},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        janv2021: {
+            registered: {allInstitution: 0, myInstitution: 0},
+            transferred_to_unit: {allInstitution: 0, myInstitution: 0},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        févr2021: {
+            registered: {allInstitution: 0, myInstitution: 0},
+            transferred_to_unit: {allInstitution: 0, myInstitution: 0},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        mars2021: {
+            registered: {allInstitution: 0, myInstitution: 0},
+            transferred_to_unit: {allInstitution: 0, myInstitution: 0},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 0, myInstitution: 0},
+            measured: {allInstitution: 0, myInstitution: 0}
+        },
+        avr2021: {
+            registered: {allInstitution: 7, myInstitution: 5},
+            transferred_to_unit: {allInstitution: 5, myInstitution: 5},
+            unfounded: {allInstitution: 0, myInstitution: 0},
+            treated: {allInstitution: 5, myInstitution: 5},
+            measured: {allInstitution: 0, myInstitution: 0}
+        }
+    },
+    totalClaimsRegisteredStatistics: 7,
+    institutionsTargeted: {
+        omg: {
+            allInstitution: 0
+        },
+        BOG: {
+            allInstitution: 1
+        },
+        SATIS: {
+            allInstitution: 6
+        },
+        DMD: {
+            allInstitution: 0
+        }
+    },
+    pointOfServicesTargeted: {
+        EXPLOITATION: {
+            myInstitution: 0
+        },
+        COMPTABILITEDMD: {
+            myInstitution: 0
+        }
+        ,
+        CORRECTIONTEST: {
+            myInstitution: 0
+        }
+        ,
+        CONSULTING: {
+            myInstitution: 0
+        }
+        ,
+        DIRECTIONDMD: {
+            myInstitution: 0
+        }
+        ,
+        PRODUCTION: {
+            myInstitution: 0
+        }
+        ,
+        INFORMATIQUEDMD: {
+            myInstitution: 0
+        }
+        ,
+        ADMINISTRATIONPLATEFORMEDMD: {
+            myInstitution: 0
+        }
+    }
+};
+
