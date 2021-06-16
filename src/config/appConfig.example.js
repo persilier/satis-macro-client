363f67b708bc5dc4d7cf58aa17f399245411fbac
dmd_estelle <estelle@dmdconsult.com>
2021-06-16 15:09:00 +0100

    add appConfig.example

---
+++ src/config/appConfig.example.js
@@ -0,0 +1,13 @@
+const appConfig =  {
+    version: "2020.1",
+    name: 'SATIS',
+    appFullName: (plan) => `SATIS ${plan} 2020.1`,
+    enterprise: 'DMD',
+    contact: '21256325',
+    timeAfterDisconnection: 8,
+    apiDomaine:`http://satismacro.develop`,
+    host: `satismacro.develop`,
+    port: 6001
+};
+
+export default appConfig;
