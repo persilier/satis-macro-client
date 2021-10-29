01ebf735b622ac2bb832b7f5d7157f7bb2de5f1e
Lewhe Onesine <onesine@dmdconsult.com>
2021-10-29 16:12:15 +0200

    Correction

--- src/views/components/kanban/ColToValidate.jsx
+++ src/views/components/kanban/ColToValidate.jsx
@@ -43,11 +43,11 @@ const ColToValidate = (props) => {
     };
 
     const filterByCategory = () => {
-        currentFilterData = currentFilterData.filter(claim => claim.claim_object.claim_category_id.indexOf(props.filterCategory.value) >= 0);
+        currentFilterData = currentFilterData.filter(claim => (claim.claim_object !== null && claim.claim_object.claim_category_id !== null) ? claim.claim_object.claim_category_id.indexOf(props.filterCategory.value) >= 0 : false);
     };
 
     const filterByObject = () => {
-        currentFilterData = currentFilterData.filter(claim => claim.claim_object_id.indexOf(props.filterObject.value) >= 0);
+        currentFilterData = currentFilterData.filter(claim => claim.claim_object_id !== null ? claim.claim_object_id.indexOf(props.filterObject.value) >= 0 : false);
     };
 
     const filterByPeriod = () => {
