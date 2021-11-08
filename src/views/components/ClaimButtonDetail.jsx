2c2bc55f159475fd0f1a842862bcaa974f7d549e
Estelle Odjo <estelle@dmdconsult.com>
2021-11-08 17:37:32 +0100

    Correction

--- src/views/components/ClaimButtonDetail.jsx
+++ src/views/components/ClaimButtonDetail.jsx
@@ -21,47 +21,55 @@ const ClaimButtonDetail = ({claim, plan}) => {
                     <div className="kt-wizard-v2__review-item">
                         <div className="kt-wizard-v2__review-item">
                             <div className="kt-wizard-v2__review-title">
-                                <h5 style={{color:"#48465b"}}>Référence</h5>
+                                <h5 style={{color: "#48465b"}}>Référence</h5>
                             </div>
                             {!claim ? null : (
                                 <div className="kt-wizard-v2__review-content">
+
                                     <strong>Référence:</strong>
                                     <span className="mx-2">{claim.reference ? claim.reference : "-"}</span><br/>
                                     <strong>Catégorie de réclamation:</strong>
-                                    <span className="mx-2">{claim.claim_object.claim_category.name ? claim.claim_object.claim_category.name["fr"] : '-'}</span><br/>
+                                    <span
+                                        className="mx-2">{(claim.claim_object && claim.claim_object.claim_category.name) ? claim.claim_object.claim_category.name["fr"] : '-'}</span><br/>
                                     <strong>Objet de réclamations:</strong>
                                     <span className="mx-2">{claim.claim_object.name["fr"]}</span><br/>
                                     <strong>Date de survenu de l'incident:</strong>
-                                    <span className="mx-2">{claim.event_occured_at ? formatDateToTimeStampte(claim.event_occured_at) : "-"}</span><br/>
+                                    <span
+                                        className="mx-2">{claim.event_occured_at ? formatDateToTimeStampte(claim.event_occured_at) : "-"}</span><br/>
                                     <strong>Lieu de survenu de l'incident:</strong>
                                     <span className="mx-2">{claim.lieu ? claim.lieu : "-"}</span><br/>
                                     {plan === "HUB" && (
                                         <>
                                             <strong>Relation entretenue avec le réclamant:</strong>
-                                            <span className="mx-2">{claim.relationship ? formatDateToTimeStampte(claim.relationship.name["fr"]) : "-"}</span><br/>
+                                            <span
+                                                className="mx-2">{claim.relationship ? formatDateToTimeStampte(claim.relationship.name["fr"]) : "-"}</span><br/>
                                         </>
                                     )}
                                     <strong>Montant réclamé:</strong>
-                                    <span className="mx-2">{claim.amount_disputed ? `${claim.amount_disputed} ${claim.amount_currency ? claim.amount_currency.name["fr"] : ''}` : "-"}</span><br/>
+                                    <span
+                                        className="mx-2">{claim.amount_disputed ? `${claim.amount_disputed} ${claim.amount_currency ? claim.amount_currency.name["fr"] : ''}` : "-"}</span><br/>
                                     <strong>Description:</strong>
                                     <span className="mx-2">
-                                        <HtmlDescription onClick={() => showModal(claim.description ? claim.description : '-')}/>
+                                        <HtmlDescription
+                                            onClick={() => showModal(claim.description ? claim.description : '-')}/>
                                     </span>
                                     {/*<span className="mx-2">{claim.description ? claim.description : "-"}</span>*/}
                                     <br/>
                                     <strong>Attente:</strong>
-                                    <span className="mx-2">{claim.claimer_expectation ? claim.claimer_expectation : "-"}</span><br/>
+                                    <span
+                                        className="mx-2">{claim.claimer_expectation ? claim.claimer_expectation : "-"}</span><br/>
                                     <strong>Est-ce une relance ?:</strong>
                                     <span className="mx-2">{claim.is_revival ? "Oui" : "Non"}</span><br/>
                                 </div>
                             )}
-                            <button id="button_modal" type="button" className="btn btn-secondary btn-icon-sm d-none" data-toggle="modal" data-target="#message_email"/>
+                            <button id="button_modal" type="button" className="btn btn-secondary btn-icon-sm d-none"
+                                    data-toggle="modal" data-target="#message_email"/>
                             <HtmlDescriptionModal title={"Description"} message={currentMessage}/>
                         </div>
 
                         <div className="kt-wizard-v2__review-item">
                             <div className="kt-wizard-v2__review-title">
-                                <h5><span style={{color:"#48465b"}}>Cible</span></h5>
+                                <h5><span style={{color: "#48465b"}}>Cible</span></h5>
                             </div>
                             {
                                 !claim ? null : (
@@ -69,11 +77,13 @@ const ClaimButtonDetail = ({claim, plan}) => {
                                         <strong>Institution:</strong>
                                         <span className="mx-2">{claim.institution_targeted.name}</span><br/>
                                         <strong>Point de service</strong>:
-                                        <span className="mx-2">{claim.unit_targeted ? claim.unit_targeted.name["fr"] : "-"}</span><br/>
+                                        <span
+                                            className="mx-2">{claim.unit_targeted ? claim.unit_targeted.name["fr"] : "-"}</span><br/>
                                         {claim.account_targeted ? (
                                             <>
                                                 <strong>Compte concerné:</strong>
-                                                <span className="mx-2">{claim.account_targeted ? claim.account_targeted.number : "-"}</span><br/>
+                                                <span
+                                                    className="mx-2">{claim.account_targeted ? claim.account_targeted.number : "-"}</span><br/>
                                             </>
                                         ) : null}
                                     </div>
@@ -83,32 +93,38 @@ const ClaimButtonDetail = ({claim, plan}) => {
 
                         <div className="kt-wizard-v2__review-item">
                             <div className="kt-wizard-v2__review-title">
-                                <h5><span style={{color:"#48465b"}}>Canaux</span></h5>
+                                <h5><span style={{color: "#48465b"}}>Canaux</span></h5>
                             </div>
                             {!claim ? null : (
                                 <div className="kt-wizard-v2__review-content">
                                     <strong>Canal de réception:</strong>
-                                    <span className="mx-2">{claim.request_channel ? claim.request_channel.name["fr"] : "-"}</span><br/>
+                                    <span
+                                        className="mx-2">{claim.request_channel ? claim.request_channel.name["fr"] : "-"}</span><br/>
                                     <strong>Canal de réponse préférentiel:</strong>
-                                    <span className="mx-2">{claim.response_channel ? claim.response_channel.name["fr"] : "-"}</span><br/>
+                                    <span
+                                        className="mx-2">{claim.response_channel ? claim.response_channel.name["fr"] : "-"}</span><br/>
                                 </div>
                             )}
                         </div>
 
                         <div className="kt-wizard-v2__review-item">
                             <div className="kt-wizard-v2__review-title">
-                                <h5><span style={{color:"#48465b"}}>Enregistré par</span></h5>
+                                <h5><span style={{color: "#48465b"}}>Enregistré par</span></h5>
                             </div>
                             {!claim ? null : (
                                 <div className="kt-wizard-v2__review-content">
                                     <strong>Nom:</strong>
-                                    <span className="mx-2">{`${(claim.created_by && claim.created_by.identite) ? claim.created_by.identite.firstname+" "+claim.created_by.identite.lastname : "-"} `}</span><br/>
+                                    <span
+                                        className="mx-2">{`${(claim.created_by && claim.created_by.identite) ? claim.created_by.identite.firstname + " " + claim.created_by.identite.lastname : "-"} `}</span><br/>
                                     <strong>Point de service:</strong>
-                                    <span className="mx-2">{(claim.created_by && claim.created_by.unit) ? claim.created_by.unit.name["fr"] : "-"}</span><br/>
+                                    <span
+                                        className="mx-2">{(claim.created_by && claim.created_by.unit) ? claim.created_by.unit.name["fr"] : "-"}</span><br/>
                                     <strong>Institution:</strong>
-                                    <span className="mx-2">{(claim.created_by && claim.created_by.institution) ? claim.created_by.institution.name : "-"}</span><br/>
+                                    <span
+                                        className="mx-2">{(claim.created_by && claim.created_by.institution) ? claim.created_by.institution.name : "-"}</span><br/>
                                     <strong>Date de réception:</strong>
-                                    <span className="mx-2">{claim.created_at ? formatDateToTimeStampte(claim.created_at) : "-"}</span><br/>
+                                    <span
+                                        className="mx-2">{claim.created_at ? formatDateToTimeStampte(claim.created_at) : "-"}</span><br/>
                                 </div>
                             )}
                         </div>
@@ -116,17 +132,28 @@ const ClaimButtonDetail = ({claim, plan}) => {
                         {claim && claim.completed_at ? (
                             <div className="kt-wizard-v2__review-item">
                                 <div className="kt-wizard-v2__review-title">
-                                    <h5><span style={{color:"#48465b"}}>Complété par</span></h5>
+                                    <h5><span style={{color: "#48465b"}}>Complété par</span></h5>
                                 </div>
                                 <div className="kt-wizard-v2__review-content">
                                     <strong>Nom:</strong>
-                                    <span className="mx-2">{`${claim.completed_by.identite ? claim.completed_by.identite.firstname+" "+claim.completed_by.identite.lastname : "-"} `}</span><br/>
+
+                                    <span
+                                        className="mx-2">{`${(claim.completed_by && claim.completed_by.identite) ? claim.completed_by.identite.firstname + " " + claim.completed_by.identite.lastname : "-"} `}</span>
+
+                                    <br/>
                                     <strong>Point de service:</strong>
-                                    <span className="mx-2">{claim.completed_by.unit ? claim.completed_by.unit.name["fr"] : "-"}</span><br/>
+
+                                    <span
+                                        className="mx-2">{(claim.completed_by && claim.completed_by.unit) ? claim.completed_by.unit.name["fr"] : "-"}</span>
+                                    <br/>
                                     <strong>Institution:</strong>
-                                    <span className="mx-2">{claim.completed_by.institution ? claim.completed_by.institution.name : "-"}</span><br/>
+
+                                    <span
+                                        className="mx-2">{(claim.completed_by && claim.completed_by.institution) ? claim.completed_by.institution.name : "-"}</span>
+                                    <br/>
                                     <strong>Date de complétion:</strong>
-                                    <span className="mx-2">{claim.completed_at ? formatDateToTimeStampte(claim.completed_at) : "-"}</span><br/>
+                                    <span
+                                        className="mx-2">{claim.completed_at ? formatDateToTimeStampte(claim.completed_at) : "-"}</span><br/>
                                 </div>
                             </div>
                         ) : null}
