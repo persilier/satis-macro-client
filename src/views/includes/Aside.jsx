6f9bfe6e04d2631d5fff20fb817a282b9dc1aaa9
Lewhe Onesine <onesine@dmdconsult.com>
2022-01-21 11:08:24 +0100

    Wording

--- src/views/includes/Aside.jsx
+++ src/views/includes/Aside.jsx
@@ -828,7 +828,7 @@ const Aside = (props) => {
                                                         <NavLink exact to="/settings/logs" className="kt-menu__item " activeClassName="kt-menu__item--active" aria-haspopup="true">
                                                             <li className="kt-menu__link ">
                                                                 <i className="kt-menu__link-bullet kt-menu__link-bullet--dot"><span/></i>
-                                                                <span className="kt-menu__link-text">Journalisation des activitées</span>
+                                                                <span className="kt-menu__link-text">Journal des activitées</span>
                                                             </li>
                                                         </NavLink>
                                                     ) : null
