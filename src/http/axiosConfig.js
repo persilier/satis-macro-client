ab111876ed3607290fc3016a025a41c77a430e7f
henoc <henochounsa@gmail.com>
2022-01-25 15:53:33 +0100

    logout api on session timeout

--- src/http/axiosConfig.js
+++ src/http/axiosConfig.js
@@ -1,13 +1,19 @@
 import appConfig from "../config/appConfig";
 import { logout } from "../helpers/function";
-import {isTimeOut} from "../helpers";
+import { isTimeOut } from "../helpers";
+import { logoutUser } from "./crud";
 
 export default function setupAxios(axios, store) {
   axios.interceptors.request.use(
     (config) => {
-      if(isTimeOut()){
-        console.log("TIME_IS_OUT!!!!")
-        logout();
+      if (isTimeOut()) {
+        logoutUser()
+          .then(({ data }) => {
+            console.log(data);
+            console.log("TIME_IS_OUT!!!!");
+            logout();
+          })
+          .catch(console.log);
       }
       const token = localStorage.getItem("token");
       config.baseURL = appConfig.apiDomaine;
@@ -26,7 +32,6 @@ export default function setupAxios(axios, store) {
       return response;
     },
     (error) => {
-    
       if (401 === error.response.status || 498 === error.response.status) {
         console.log("CHECK WITH BACKEND EXPIRED TOKEN CODE");
       } else {
