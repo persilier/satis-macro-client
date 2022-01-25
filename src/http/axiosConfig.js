4734c634d57eb3b08d4aa16c0ad22b5e1009fcf6
magnoliaa <magnolia@dmdconsult.com>
2022-01-25 12:27:49 +0100

    Correction

--- src/http/axiosConfig.js
+++ src/http/axiosConfig.js
@@ -1,9 +1,7 @@
 import appConfig from "../config/appConfig";
 import { logout } from "../helpers/function";
-import {isTimeOut} from "../helpers";
 
 export default function setupAxios(axios, store) {
-<<<<<<< HEAD
     axios.interceptors.request.use(
         (config) => {
             const token = localStorage.getItem("token");
@@ -12,27 +10,12 @@ export default function setupAxios(axios, store) {
             if (token) {
                 config.headers.Authorization = `Bearer ${token}`;
             }
-=======
-  axios.interceptors.request.use(
-    (config) => {
-      if(isTimeOut()){
-        console.log("TIME_IS_OUT!!!!")
-        logout();
-      }
-      const token = localStorage.getItem("token");
-      config.baseURL = appConfig.apiDomaine;
-      config.headers.post["Content-Type"] = "application/json";
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
->>>>>>> 162be4624f8b5563c241f4d43ddeca566d7003bb
 
             return config;
         },
         (err) => Promise.reject(err)
     );
 
-<<<<<<< HEAD
     axios.interceptors.response.use(
         (response) => {
             return response;
@@ -52,19 +35,4 @@ export default function setupAxios(axios, store) {
             }
         }
     );
-=======
-  axios.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-
-      if (401 === error.response.status || 498 === error.response.status) {
-        console.log("CHECK WITH BACKEND EXPIRED TOKEN CODE");
-      } else {
-        return Promise.reject(error);
-      }
-    }
-  );
->>>>>>> 162be4624f8b5563c241f4d43ddeca566d7003bb
 }
