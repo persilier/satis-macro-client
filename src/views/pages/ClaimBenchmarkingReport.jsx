239cab34a902ae7e307b1dc829d9f5dd56dd64ab
Kilian Vitou <stagiairerd@dmdconsult.com>
2022-04-04 17:15:03 +0100

    benchmarking report

---
+++ src/views/pages/ClaimBenchmarkingReport.jsx
@@ -0,0 +1,13 @@
+const ClaimBenchmarkingReport = (props) => {
+
+}
+
+const mapStateToProps = state => {
+    return {
+        plan: state.plan.plan,
+        userPermissions: state.user.user.permissions,
+        activePilot: state.user.user.staff.is_active_pilot
+    };
+};
+
+export default connect(mapStateToProps)(ClaimBenchmarkingReport);
