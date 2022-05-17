2918759ed693df7268be913804b4a3db70081058
kilian-dmd <kilian@dmdconsult.com>
2022-05-17 17:28:22 +0100

    add revivals historic page

--- src/views/pages/HistoricRevivals.jsx
+++ src/views/pages/HistoricRevivals.jsx
@@ -10,6 +10,7 @@ import EmptyTable from "../components/EmptyTable";
 import HtmlDescriptionModal from "../components/DescriptionDetail/HtmlDescriptionModal";
 import {NUMBER_ELEMENT_PER_PAGE} from "../../constants/dataTable";
 import Pagination from "../components/Pagination";
+import Select from "react-select";
 
 loadCss("/assets/plugins/custom/datatables/datatables.bundle.css");
 
@@ -124,6 +125,43 @@ const HistoricRevivals = (props) => {
                             ) : (
                                 <div className="kt-portlet__body">
                                     <div id="kt_table_1_wrapper" className="dataTables_wrapper dt-bootstrap4">
+
+
+                                        <div className="text-center m-auto col-xl-4 col-lg-12 order-lg-3 order-xl-1">
+                                            <div className="" style={{marginBottom: "30px"}}>
+                                                <div className="kt-portlet__body" style={{padding: "10px 25px"}}>
+                                                    <div className="kt-widget6">
+                                                        <div className="kt-widget6__body">
+                                                            <div className="kt-widget6__item row" style={{padding: "0.5rem 0"}}>
+                                                                <div className="col-lg-1">Label</div>
+                                                                <div className={"col-lg-9"}>
+                                                                    <Select
+                                                                        value={null}
+                                                                        isClearable
+                                                                        placeholder={"Veuillez"}
+                                                                        options={[]}
+                                                                    />
+                                                                                                                    {
+           /*                                         error.unit_targeted_id.length ? (
+                                                        error.unit_targeted_id.map((error, index) => (
+                                                            <div key={index}
+                                                                 className="invalid-feedback">
+                                                                {error}
+                                                            </div>
+                                                        ))
+                                                    ) : null*/
+                                                }
+                                                                </div>
+                                                                <div className="col-lg-2">
+                                                                    <button type="submit" onClick={(e) => console.log(e)} className="btn btn-primary">{t("Filtrer")}</button>
+                                                                </div>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
 
                                         <div className="row">
                                             <div className="col-sm-6 text-left">
