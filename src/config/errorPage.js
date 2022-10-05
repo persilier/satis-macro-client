832e10f33d5da17507a8df7b190983fe9f988934
magnoliaa <magnolia@dmdconsult.com>
2022-10-05 16:06:05 +0100

    fix errors pages for 401 403 & 500 errors

--- src/config/errorPage.js
+++ src/config/errorPage.js
@@ -5,7 +5,7 @@ export const ERROR_401 = "/error401";
 
 
 export const redirectErrorPage = code => {
-    if (code === "403" || code === "500")
+    if (code === 403 || code === 500)
         window.location.href = `/error${code}`;
     else if (code === 401)
         logout();
