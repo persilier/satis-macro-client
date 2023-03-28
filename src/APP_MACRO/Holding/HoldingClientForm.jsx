51a7b4f495f98e0f6b2c1783fb6195d71f829d12
Wayd26 <wayedoun@gmail.com>
2023-03-28 09:36:54 +0100

    add telephone to holding form add client

--- src/APP_MACRO/Holding/HoldingClientForm.jsx
+++ src/APP_MACRO/Holding/HoldingClientForm.jsx
@@ -1104,7 +1104,7 @@ const HoldingClientForm = (props) => {
                                         className="dropdown-item"
                                         style={{ cursor: "pointer" }}
                                       >
-                                        <strong>{el?.fullName || el.client?.identite?.fullName}</strong>
+                                        <strong>{el?.fullName || el.client?.identite?.fullName} / {el.client?.identite?.telephone}</strong>
                                       </span>
                                     ))}
                                   </div>
